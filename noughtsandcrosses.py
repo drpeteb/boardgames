Fixed scaling and learning rate for learning N&C player. Can now learn not to lose against expert.

noughtsandcrosses.py
@@ -375,8 +375,8 @@ class LearningNoughtsAndCrossesPlayer(Player):
         self.neural_net = BoardgameNeuralNet(num_inputs=9,
                                              num_hidden_layers=2,
                                              num_hidden_units=[200,200],
-                                             step_size=1E-1,
-                                             regulariser=1E-4)
+                                             step_size=3E-1,
+                                             regulariser=1E-2)
 
     def move(self, board):
         """
@@ -410,7 +410,7 @@ class LearningNoughtsAndCrossesPlayer(Player):
 
             # Estimate probability of winning
             state = board.turn * bd.state.flatten()[np.newaxis,:]
-            prob[mm,:] = self.neural_net.predict(state)
+            prob[mm,:] = self.neural_net.predict(state/16.0)
             
             print("Log-probability of 0/+1/-1 victory if I make move {} "
                   "is {}/{}/{}.".format(state, *prob[mm,:]))
@@ -423,9 +423,8 @@ class LearningNoughtsAndCrossesPlayer(Player):
                 break
 
         if move is None:
-            # Select the move which maximises prob(win)/prob(lose)
-            prob_diff = prob[:,board.turn]-prob[:,-board.turn]
-            move = legal_moves[np.argmax(prob_diff)]
+            # Select the move which minimises the probability of losing
+            move = legal_moves[np.argmin(prob[:,-board.turn])]
 
         # Store the board for learning later
         board.move(move)
@@ -443,7 +442,7 @@ class LearningNoughtsAndCrossesPlayer(Player):
         outputs = winner*np.ones(states.shape[0], dtype=int)
 
         # Update the net
-        self.neural_net.update(states, outputs)
+        self.neural_net.update(states/16.0, outputs)
 
     def notify(self, event, info):
         """

run_noughts_and_crosses.py
@@ -8,7 +8,7 @@ from noughtsandcrosses import (NoughtsAndCrossesGame,
                                LearningNoughtsAndCrossesPlayer,
                                ExpertNoughtsAndCrossesPlayer)
 
-np.random.seed(seed=1)
+np.random.seed(seed=3)
 
 #player1 = HumanNoughtsAndCrossesPlayer("Pete")
 #player2 = HumanNoughtsAndCrossesPlayer("Katy")
@@ -19,8 +19,8 @@ player2 = LearningNoughtsAndCrossesPlayer("Franklin")
 
 result = []
 
-num_games = 1000
-num_eval = 100
+num_games = 500
+num_eval = 50
 
 for gg in range(num_games):
     game = NoughtsAndCrossesGame([player1,player2])
@@ -35,6 +35,13 @@ for gg in range(num_games):
 #    if game.winner == "Franklin":
 #        raise BoardgameError("Horatio should be unbeatable!!")
 
+if result[0] == 1:
+    print("First game was won by Franklin.")
+elif result[0] == 0:
+    print("First game was drawn by Franklin.")
+elif result[0] == -1:
+    print("First game was lost by Franklin.")
+    
 print("Franklin won {}\% of the first {} games.".format(
                     100*np.mean(np.array(result[:num_eval]) == 1), num_eval))
 print("Franklin drew {}\% of the first {} games.".format(